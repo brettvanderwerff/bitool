c88ac6b9bf01fad9cb78077e3e7a43c166b02f83
Brett.Vanderwerff <vande060@gmail.com>
2018-09-27 20:00:08 -0700

    added hashing and peer id

--- bitool/src/tracker.py
+++ bitool/src/tracker.py
@@ -1,9 +1,12 @@
 from bitool.src.torrent_file import TorrentFile
+import bencoder
+import hashlib
 import random
 import socket
 import struct
 
-class ConnectRequest():
+
+class ConnectReq():
     '''
     Class representing the connection request to the tracker
     '''
@@ -20,7 +23,7 @@ class ConnectRequest():
         Send request to tracker for a connection ID
         '''
         self.sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        self.sock.connect((ConnectRequest.TRACKER_URL, ConnectRequest.TRACKER_PORT))
+        self.sock.connect((ConnectReq.TRACKER_URL, ConnectReq.TRACKER_PORT))
         protocol_id = 0x41727101980
         action = 0
         packet = struct.pack(">qii", protocol_id, action, self.transaction_id)
@@ -39,8 +42,33 @@ class ConnectRequest():
         self.send_request()
         self.recv_connec_id()
 
+class AnnounceReq():
+    def __init__(self, torrent_file):
+        self.torrent_file = TorrentFile(torrent_file)
+
+    def hash(self):
+        '''
+        Generates SHA1 hash of the torrent info value.
+        :return: hash
+        '''
+        info = self.torrent_file.info
+        encoded_info = bencoder.encode(info)
+        hash = hashlib.sha1(encoded_info)
+        return hash
+
+    def peer_id(self):
+        '''
+        Generates a 20 byte peer ID as per bit torrent protocol: http://www.bittorrent.org/beps/bep_0020.html
+        :return:
+        '''
+        peer_id = ['-AZ2060-']
+        for i in range(12):
+            peer_id.append(str(random.randrange(1, 9)))
+        return bytearray(''.join(peer_id), 'utf-8')
+
+
 if __name__ == "__main__":
-    connec_req = ConnectRequest()
+    connec_req = ConnectReq()
     connec_req.connect()
     print(connec_req.connection_id)
 
