9491b935a996ac858e65b1c371af9ea047ae890d
Brett Vanderwerff <vande060@gmail.com>
2018-10-18 20:49:26 -0700

    finish download file dict

--- bitool/src/download_file.py
+++ bitool/src/download_file.py
@@ -12,9 +12,26 @@ class DownloadFile():
         self.last_piece = torrent_file.length % torrent_file.piece_length
 
         self.template = {}
-        for piece in range(torrent_file.piece_count):
+        for piece_count, piece in enumerate(range(torrent_file.piece_count)):
+            self.template['piece_{}'.format(piece_count)] = {}
             for factor, block in enumerate(range(self.blocks_per_piece)):
-                self.template['peice_{}_block_{}'.format(piece, block)] = self.block_size + factor * self.block_size
+                self.template['piece_{}'.format(piece_count)]['block_{}'.format(block)] = self.block_size + factor * self.block_size
+
+
+        if self.last_piece <= 2 ** 14:
+            self.template['peice_{}'.format(piece_count + 1)] = {'block_0' : self.last_piece}
+
+
+
+
+
+
+
+
+
+
+
+        #ToDo add logic for if last piece is larger than 2 ** 14 bytes
 
     DOWNLOADED = 0
     UPLOADED = 0
