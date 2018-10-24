fb2068b055e870a8f6f1ea5e6e4f033ccf86473f
Brett Vanderwerff <vande060@gmail.com>
2018-10-23 17:48:06 -0700

    workaround import error

--- peer_connection.py
+++ peer_connection.py
@@ -1,11 +1,118 @@
 import socket
 import struct
-import get_ip
+import bencoder
 import random
 import magnet_link
 from bitstring import BitArray
 import os
 import download_file
+import hashlib
+import binascii
+import sys
+
+
+magnet = magnet_link.MagnetLink(sys.argv[1])
+torrent = download_file.DownloadFile(magnet)
+
+def parse_trackers():
+    tracker_list = []
+    for tracker in magnet.trackers:
+        tracker = tracker.decode('utf-8').split('/')[-1]
+        tracker_url = tracker.split(":")[0]
+        tracker_port = int(tracker.split(":")[1])
+        tracker_list.append((tracker_url, tracker_port))
+    tracker_list.append(('tracker.coppersurfer.tk', 6969)) # Fall back on known to work tracker
+    return tracker_list
+
+def build_request():
+    protocol_id = 0x41727101980
+    action = 0
+    transaction_id = random.randrange(1, 1000)
+    request = struct.pack(">qii", protocol_id, action, transaction_id)
+    return request
+
+def gen_hash():
+    info = magnet.info
+    encoded_info = bencoder.encode(info)
+    return hashlib.sha1(encoded_info)
+
+def gen_peer_id():
+    peer_id = ['-AZ2060-']
+    for i in range(12):
+        peer_id.append(str(random.randrange(1, 9)))
+    return bytearray(''.join(peer_id), 'utf-8')
+
+def build_announce_req(connection_id, hash, peer_id):
+    binary_hash = binascii.a2b_hex(hash.hexdigest())
+    action = 1
+    transaction_id = random.randrange(1, 1000)
+    downloaded = 0
+    uploaded = 0
+    event = 0
+    ip = 0
+    left = magnet.length
+    key = random.randrange(1, 1000)
+    num_want = -1
+    port = 6681
+    request = struct.pack(">qii20s20sqqqiiiih",
+                         connection_id,
+                         action,
+                         transaction_id,
+                         binary_hash,
+                         peer_id,
+                         downloaded,
+                         left,
+                         uploaded,
+                         event,
+                         ip,
+                         key,
+                         num_want,
+                         port)
+    return request
+
+
+def communicate_DGRAM(target, message):
+    sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    sock.settimeout(15)
+    sock.connect(target)
+    sock.send(message)
+    response = sock.recv(4096)
+    sock.close()
+    return response
+
+def connect_tracker(tracker_list, request, hash, peer_id):
+    for tracker in tracker_list:
+        try:
+            print('Connecting to trackers...')
+            response = communicate_DGRAM(tracker, request)
+            unpacked_response = struct.unpack(">iiq", response)
+            connection_id = unpacked_response[2]
+            print('Connection to tracker formed')
+            announce_req = build_announce_req(connection_id, hash, peer_id)
+            print('Receiving list of peers..')
+            response = communicate_DGRAM(tracker, announce_req)
+            return response
+        except (ConnectionRefusedError, socket.timeout) as e:
+            continue
+
+def parse_ips(response):
+    ips_ports = []
+    for offset in range(20, len(response), 6):
+        raw_ip = struct.unpack_from(">BBBB", response, offset)
+        ip = '.'.join([str(i) for i in raw_ip])
+        port = struct.unpack_from(">H", response, offset + 4)[0]
+        ips_ports.append((ip, port))
+    print("Peer list received")
+    return ips_ports
+
+
+def gen_ips():
+    tracker_list = parse_trackers()
+    request = build_request()
+    hash = gen_hash()
+    peer_id = gen_peer_id()
+    response = connect_tracker(tracker_list, request, hash, peer_id)
+    return parse_ips(response)
 
 
 def gen_peer_id():
@@ -81,8 +188,11 @@ def parse_response(sock):
         raise TimeoutError
 
     elif id == 1:
-        print('Message type: unchoked, peer will allow sharing')
-        recv_unchoked(sock)
+        try:
+            print('Message type: unchoked, peer will allow sharing')
+            recv_unchoked(sock)
+        except:
+            print('Error peer dropped')
 
     elif id == 5:
         # Message type: bitfield
@@ -102,8 +212,6 @@ def parse_response(sock):
     else:
         print('Error communicating with peer, dropping connection.')
         raise TimeoutError
-
-
 
 
 def eval_bitfield(payload_length, sock):
@@ -138,7 +246,7 @@ def recv_piece(payload_length, sock):
         torrent.bytes += piece
         torrent.have[torrent.cur_index][torrent.cur_block] = True
         progress = str(round((sum([sum(piece) for piece in torrent.have]) / sum([len(piece) for piece in torrent.have]) * 100), 2))
-        print("receiving data from peer.. " + progress + " % complete")
+        print("receiving data from peer... " + progress + " % complete")
 
 
 
@@ -174,17 +282,14 @@ def connect_peers(ips, hash):
         except (TimeoutError, OSError, BrokenPipeError, struct.error) as e:
             sock.close()
 
-def run(link):
-    magnet = magnet_link.MagnetLink(link)
 
-    torrent = download_file.DownloadFile(magnet)
 
+def run():
     while torrent.done == False:
-        ips = get_ip.gen_ips(magnet)
-        hash = get_ip.gen_hash(magnet)
+        ips = gen_ips()
+        hash = gen_hash()
         connect_peers(ips, hash)
 
 
-if __name__ == "__main__":
-    run("link")
+
 
